94a40a0ec5c8ea5e614f65bd86a3e6c80780fd30
Maranatha Odai <moodai@st.ug.edu.gh>
2025-11-03 15:26:08 +0000

    chore: remove .mastra build artifacts and add to .gitignore

--- .mastra/.build/bundler-config.mjs
+++
@@ -1,4 +0,0 @@
-var mastra$1 = mastra;
-const bundler = {};
-
-export { bundler, mastra$1 as default };
--- .mastra/output/telemetry-config.mjs
+++
@@ -1,4 +0,0 @@
-var mastra$1 = mastra;
-const telemetry = {};
-
-export { mastra$1 as default, telemetry };
