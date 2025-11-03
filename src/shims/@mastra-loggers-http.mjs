b4a91b5443c41e826ab907ad797f8ca8dda53544
Maranatha Odai <moodai@st.ug.edu.gh>
2025-11-03 14:16:54 +0000

    ...

---
+++ src/shims/@mastra-loggers-http.mjs
@@ -0,0 +1,6 @@
+// Intentionally export an empty module to prevent bundlers from generating
+// self-referential reexports for HttpTransport. The app does not use this.
+export const HttpTransport = undefined;
+export default {};
+
+
